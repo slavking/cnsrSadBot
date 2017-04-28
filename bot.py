b193cba60f30fb02b02f1eb6bc64ffc5cffca9be
cnsr <dmytrenkoyar@gmail.com>
2017-04-29 01:43:53 +0300

    removed some imports, other unneeded shit, added bot commands in separate file

--- bot.py
+++ bot.py
@@ -1,24 +1,19 @@
 #remade by cnsr based on livechan API and anna bot code
 #added telegram integration
-#bot starts receiving incoming data only after sending any outcoming message
+###bot starts receiving incoming data only after sending any outcoming message
 #kinda works but not really
 
+#imports
 from time import sleep
 from api import *
 import telebot
 import re
-import sys, os
+import sys
 import random
-from datetime import datetime
+#from datetime import datetime
 import config
 from urllib2 import urlopen
-
-#im not even sure if i need those imports
-try:
-	from urllib.request import urlretrieve
-except ImportError:
-	from urllib import urlretrieve
-
+import hbot
 import requests
 
 tbot = telebot.TeleBot(config.token)
@@ -31,11 +26,7 @@ if (len(sys.argv) < 2):
 #if you did it just werks
 channel = sys.argv[1]
 
-#this should be removed?
-# globals
-users = {}
-
-#writes image on disk so it could be read by api later
+#writes image on disk so it could be readen by api later
 def send_image(url):
 	url = url.replace('/home/ph/livechan-js/public/',
 	'https://sadchan.sytes.net/')
@@ -44,6 +35,7 @@ def send_image(url):
 	f.close()
 
 def process_chat(*args):
+	#uncomment for debug:
 	#print(args)
 	try:
 		#get vars from args
@@ -51,42 +43,47 @@ def process_chat(*args):
 		message = args[0]["body"]
 		name = args[0]["name"]
 		count = str(args[0]["count"])
-		convo = args[0]["convo"]#"General"
+		convo = args[0]["convo"]#everything will be only posted to "General"
 		country_name = args[0]["country_name"]
 		country = args[0]["country"]
 		
+		help_msg = 'no help message defined'
+		
 		#for ekb and those who don't have names
+		#todo - add separate file dictionary for idents + names of nameless		
 		if ident == ('$2a$10$mAM0oYrjp0bCHFDsGaiB.etG2OGec9NtgRDtoctQiWUZ4zww1nZ9C'):
 			name = "ekb-cuck"
 		if name == '':
 			name = "nameless faggot"
-			
+		
+		#bot commands
+		for (k,v) in hbot.answers.iteritems():
+			if re.match(k,message):
+				help_msg = hbot.answers[k]
+				out_msg = '>>' + count + '\n' + help_msg
+				post_chat(out_msg, channel, name = config.name,trip = config.Trip,convo = 'General', file = '')
+				
 		#handles text messages only, text + photo will only be handled as photo by next handler
 		@tbot.message_handler(func=lambda incM: True)
 		def handle_text(incM):
-			#print(incM.text)
-			post_chat(incM.text, channel, name = config.name, trip = config.Trip, convo="General",file = '')
-			print(config.name + '\n' + incM.text)
+			post_chat(incM.text, channel, name = config.name,trip = config.Trip, convo="General",file = '')
+		
 		#only handles photos, doesnt work with text
-		#todo - add text + photo handling
-		@tbot.message_handler(content_types=['photo'])
+		@tbot.message_handler(content_types=['photo','text'])
 		def handle_image(message):
 			file_id = message.photo[-1].file_id
 			imageIn = tbot.get_file(file_id)
 			image_file = requests.get('https://api.telegram.org/file/bot' + config.token + '/' + imageIn.file_path)
-			#print(image_file)
 			with open('in.jpg','wb') as f:
 				f.write(image_file.content)
-				f.close()
 			post_chat('',channel, name=config.name,trip = config.Trip, convo = 'General', file = 'in.jpg')
 		
-		#only sends posts from General conversation
+		#only sends posts from 'General' conversation
 		if convo == "General":
 			if "image" in args[0].keys():
 				out_image = args[0]["image"]
 			else:
 				out_image = ''
-			out_message = ""
 			
 			msg = name + ":\n" + message
 			
@@ -109,11 +106,13 @@ try:
 	login(callback=process_chat)
 	join_chat(channel)
 	print('Joined chat')
+	#init msg to make bot work
+	tbot.send_message(config.user_id, 'Joined chat.')
 except Exception as e:
 	print("Connection failed. Check your internet connection. Error message:")
 	print(e)
 
-#makes bot wort and tbot poll endlessly(at least while no errors occure)
+#makes bot work and tbot polls endlessly(at least while no errors occure)
 while 1:
 	sleep(10)
 	tbot.polling(none_stop = True)
---
+++ hbot.py
@@ -0,0 +1,5 @@
+#botanswers
+answers = {
+'.hbot':'Welcome to hohilbot, this message is really helpful.',
+'.habout':'This bot is completely random because it might work but also might not.'
+}
